c51d7fa7cfa1ad9c39b7e6cd4c01dc9dce0f16c6
Ken Jenney <kjenney@gmail.com>
2026-05-15 22:07:16 -0400

    No need to use run.sh

--- cmd/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Convenience script to run the chatbot with the virtual environment activated
-
-# Activate virtual environment
-#source venv/bin/activate
-
-# Run the chatbot
-python3 chatbot_agent.py
-#
